2f30209dd687359f1235c25ddc2ada54fed0c23e
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-22 11:06:02 +0700

    Advanced Node and Express - Authentication Strategies| Local Strategy

--- server.js
+++ server.js
@@ -7,8 +7,8 @@ const ObjectID = require('mongodb').ObjectID;
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
 const session = require('express-session');
 const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
 const app = express();
-
 fccTesting(app); //For FCC testing purposes
 app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
@@ -51,6 +51,19 @@ myDB(async client => {
             done(null, doc);
         });
     });
+
+    passport.use(new LocalStrategy(
+        function(username, password, done) {
+            myDataBase.findOne({ username: username }, function(err, user) {
+                console.log('User ' + username + ' attempted to log in.');
+                if (err) { return done(err); }
+                if (!user) { return done(null, false); }
+                if (password !== user.password) { return done(null, false); }
+                return done(null, user);
+            });
+        }
+    ));
+
 }).catch(e => {
     app.route('/').get((req, res) => {
         res.render('pug/index', { title: e, message: 'Unable to login' });
