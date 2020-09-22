b811c31d7241a6efcabbdd4b3eed56e8b7ad9ae5
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-22 16:58:12 +0700

    Advanced Node and Express - Implementation of Social Authentication II

--- auth.js
+++ auth.js
@@ -1,5 +1,6 @@
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const GitHubStrategy = require('passport-github').Strategy;
 const bcrypt = require('bcrypt');
 const ObjectID = require('mongodb').ObjectID;
 
@@ -27,6 +28,17 @@ module.exports = function(app, myDataBase) {
                 }
                 return done(null, user);
             });
+        }
+    ));
+
+    passport.use(new GitHubStrategy({
+            clientID: process.env.GITHUB_CLIENT_ID,
+            clientSecret: process.env.GITHUB_CLIENT_SECRET,
+            callbackURL: "https://hidden-wildwood-51126.herokuapp.com/auth/github/callback"
+        },
+        function(accessToken, refreshToken, profile, cb) {
+            console.log(profile);
+            //Database logic here with callback containing our user object
         }
     ));
 }
