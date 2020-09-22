9b8865b7bc459c7d590ec874837c8cc701b558dd
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-22 09:40:36 +0700

    Advanced Node and Express - Set up Passport

--- server.js
+++ server.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 const express = require("express");
 const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
-
+const session = require('express-session');
+const passport = require('passport');
 const app = express();
 
 fccTesting(app); //For FCC testing purposes
@@ -15,6 +16,17 @@ app.use(express.urlencoded({ extended: true }));
 //Set View Engine
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug')
+
+//Session and passport
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 //ROUTES
 app.route("/").get((req, res) => {
