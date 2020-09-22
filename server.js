b0457f6e4d1da4a7ba4ca815f203f2ed4d275f9c
vhk.forwork@gmail.com <vhk.forwork@gmail.com>
2020-09-22 09:25:27 +0700

    init project

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 "use strict";
-require('dotenv').config()
+const path = require('path');
+require('dotenv').config();
 const express = require("express");
 const myDB = require('./connection');
 const fccTesting = require("./freeCodeCamp/fcctesting.js");
@@ -11,11 +12,16 @@ app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+//Set View Engine
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug')
+
+
 app.route("/").get((req, res) => {
-  //Change the response to render the Pug template
-  res.render('Load your view here');
+    //Change the response to render the Pug template
+    res.render('pug/index');
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("Listening on port " + process.env.PORT);
+    console.log("Listening on port " + process.env.PORT);
 });
